aff741d54a899277c6b5e9f7f9b005af4d694d8a
chen14 <396820974@qq.com>
2017-12-19 19:40:05 +0800

    no message

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({
-      url: "https://studygolang.com?url=" + encodeURIComponent(tab.url) + "&title=" + encodeURIComponent(tab.title) + "&platform=chrome_extension"
+      url: "https://studygolang.com/resources/new?url=" + encodeURIComponent(tab.url) + "&title=" + encodeURIComponent(tab.title) + "&platform=chrome_extension"
     });
   });
